f42493c9b28df095be84eb2b3d65f21e55e53351
ÇAĞATAY KARASU <61315022+cgtykarasu@users.noreply.github.com>
2021-05-13 23:02:41 +0300

    3rd Lecture - 28.04.2021

---
+++ lecture03.1-oopIntro/ProductManager.java
@@ -0,0 +1,7 @@
+package day03_1oopIntro;
+
+public class ProductManager {
+    public void addToCart(Product product) {
+        System.out.println("Sepete eklendi :" + product.getName());
+    }
+}
